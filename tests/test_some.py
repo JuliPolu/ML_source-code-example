a037d13b4d28b72f8ee81f16148a0c0de0325b90
JuliPolu <juli.polus@gmail.com>
2025-04-20 16:04:46 +0300

    add pre-commit to repo

--- tests/test_some.py
+++ tests/test_some.py
@@ -1,3 +1,7 @@
+"""
+This module contains tests for the some_module module.
+"""
+
 from typing import List
 
 import pytest
@@ -5,16 +9,15 @@ import pytest
 from src.some import sum_of_two_numbers_list
 
 
-@pytest.mark.parametrize("input_list, expected_output", 
-                         [
-                             ([1, 2, 3], 6),
-                             ([1, 2, 3, 4, 5], 15),
-                             ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 55)
-                         ])
-
-
+@pytest.mark.parametrize(
+    "input_list, expected_output",
+    [([1, 2, 3], 6), ([1, 2, 3, 4, 5], 15), ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 55)],
+)
 def test_sum_of_two_numbers_list(input_list: List[float], expected_output: float):
     """
     Test the sum_of_two_numbers_list function.
+    Parameters:
+        input_list: List[float] - The list of numbers to sum.
+        expected_output: float - The expected output of the sum.
     """
     assert sum_of_two_numbers_list(input_list) == expected_output
