99f0386ed817a304edc31bfd579d1c3eaddfaa06
JuliPolu <juli.polus@gmail.com>
2025-04-20 15:43:57 +0300

    feat: Add a new module with a function to sum a list of numbers

---
+++ src/some.py
@@ -0,0 +1,19 @@
+"""
+This is a module that does something.
+"""
+
+from typing import List
+
+
+def sum_of_two_numbers_list(numbers: List[float]) -> float:
+    """
+    This function returns the sum of a list of numbers.
+
+    Parameters:
+        numbers: List[float] - A list of numbers to sum.
+
+    Returns:
+        float - The sum of the numbers.
+    """
+
+    return sum(numbers)
